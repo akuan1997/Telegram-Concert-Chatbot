a3af0ae22b94a4fb886255ae3555fb998eb35a72
akuan1997 <akuan10cc@gmail.com>
2024-05-14 03:07:42 +0800

    整合前

--- get_concert_info.py
+++ get_concert_info.py
@@ -2918,19 +2918,38 @@ def get_kktix(website, json_filename, txt_filename):
 
 
 def threads_start():
+    # thread_era.start()
+    # thread_livenation.start()
+    # thread_indievox.start()
+    # thread_kktix.start()
+    # threading_ticketplus.start()
+    thread_kktix.start()
+    thread_kktix.join()
+
+    success_msg("KKTIX")
+
     thread_era.start()
     thread_livenation.start()
+    thread_era.join()
+    thread_livenation.join()
+
+    success_msg("Era, Livenation")
+
     thread_indievox.start()
-    thread_kktix.start()
     threading_ticketplus.start()
 
-
-def threads_join():
-    thread_era.join()
     thread_indievox.join()
-    thread_kktix.join()
-    thread_livenation.join()
     threading_ticketplus.join()
+
+    success_msg("Indievox, Ticketplus")
+
+
+# def threads_join():
+#     thread_era.join()
+#     thread_indievox.join()
+#     thread_kktix.join()
+#     thread_livenation.join()
+#     threading_ticketplus.join()
 
 
 # def threads_integration():
@@ -3530,69 +3549,39 @@ def price_in_order(json_filename):
 
 
 def get_latest_concert_info(json_filename):
-    reset_failure_log()
-
-    thread_kktix.start()
-    thread_kktix.join()
-
-    success_msg("KKTIX")
-
-    thread_era.start()
-    thread_livenation.start()
-    thread_era.join()
-    thread_livenation.join()
-
-    success_msg("Era, Livenation")
-
-    thread_indievox.start()
-    threading_ticketplus.start()
-
-    thread_indievox.join()
-    threading_ticketplus.join()
-
-    success_msg("Indievox, Ticketplus")
-    success_msg("Playwright")
-    # threads_start()  # start the threads
-    # threads_join()  # wait for all the threads finish
-    # print('--- Playwright finished ---')
-
-    """"""
-    merge_json_data(concert_json_filenames, json_filename)  # combine all the website json file into the second argument
-    success_msg("Website jsons merged")
-    # print('--- Json merged ---')
-
-    move_concert_files(concert_json_filenames)  # move each website json file to folder "website_jsons"
-    success_msg("Website jsons moved")
-    # print('--- Json moved ---')
-
-    # delete_files(concert_today)  # nothing to be deleted right now, can uncomment it in the future
-    # print('--- Files deleted ---')
-
-    get_city_from_stadium(json_filename)  # open the json file, and fill it the city according to the address
-    success_msg("Get city from loc")
-    # print('--- Get all city ---')
-
-    json_in_order(json_filename)  # sort the json file according performance time
-    success_msg("Json in order")
-    # print('--- Json in order ---')
-
-    price_str_to_int(json_filename)  # price, if str -> int
-    success_msg("Replaced str with int for all str")
-    # print('--- Replaced str with int for all str! ---')
-
-    price_in_order(json_filename)  # price in order, start from the most highest price
-    success_msg("Price in order")
-    # print('--- Price in order ---')
-
-    validate_pin(json_filename)
-    success_msg("Pin validated")
-
-    success_msg("--- zh okay ---")
+    # reset_failure_log()
+    # threads_start()
+    # success_msg("Playwright")
+    # # print('--- Playwright finished ---')
+    # """"""
+    # merge_json_data(concert_json_filenames, json_filename)  # combine all the website json file into the second argument
+    # success_msg("Website jsons merged")
+    # # print('--- Json merged ---')
+    # move_concert_files(concert_json_filenames)  # move each website json file to folder "website_jsons"
+    # success_msg("Website jsons moved")
+    # # print('--- Json moved ---')
+    # # delete_files(concert_today)  # nothing to be deleted right now, can uncomment it in the future
+    # # print('--- Files deleted ---')
+    # get_city_from_stadium(json_filename)  # open the json file, and fill it the city according to the address
+    # success_msg("Get city from loc")
+    # # print('--- Get all city ---')
+    # json_in_order(json_filename)  # sort the json file according performance time
+    # success_msg("Json in order")
+    # # print('--- Json in order ---')
+    # price_str_to_int(json_filename)  # price, if str -> int
+    # success_msg("Replaced str with int for all str")
+    # # print('--- Replaced str with int for all str! ---')
+    # price_in_order(json_filename)  # price in order, start from the most highest price
+    # success_msg("Price in order")
+    # # print('--- Price in order ---')
+    # validate_pin(json_filename)
+    # success_msg("Pin validated")
+    # success_msg("--- zh okay ---")
     # print(f'\n------------------\nzh okay!\n------------------\n')
     # delete_past_ticketing_time(concert_all_data)  # delete past ticketing time
     # print('--- Delete all past ticketing time ---')
     # """"""
-    # new_concerts, plus_concerts = get_new_old(json_filename)
+    new_concerts, plus_concerts = get_new_old(json_filename)
     # print(f"new_concerts = {new_concerts}")
     # print(f"plus_concerts = {plus_concerts}")
     # shutil.move(json_filename, "concert_jsons")
--- get_concert_new_old.py
+++ get_concert_new_old.py
@@ -264,16 +264,16 @@ def check_each_info(new_data, old_data, all_data):
                 # with open('concert_zh.json', 'w', encoding='utf-8') as file:
                 #     json.dump(all_data, file, indent=4, ensure_ascii=False)
     """"""
-    with open('concert_pin_postid.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    for pin in change_pins:
-        for i in range(len(all_data)):
-            if all_data[i]['pin'] == pin:  # 這個pin在all_data的第i個位置
-                for line in lines:
-                    line_pin = line.split('|||')[1].replace('\n', '')
-                    if pin == line_pin:
-                        post_id = line.split('|||')[0]
-                        update_post_content(post_id, all_data[i])
+    # with open('concert_pin_postid.txt', 'r', encoding='utf-8') as f:
+    #     lines = f.readlines()
+    # for pin in change_pins:
+    #     for i in range(len(all_data)):
+    #         if all_data[i]['pin'] == pin:  # 這個pin在all_data的第i個位置
+    #             for line in lines:
+    #                 line_pin = line.split('|||')[1].replace('\n', '')
+    #                 if pin == line_pin:
+    #                     post_id = line.split('|||')[0]
+    #                     update_post_content(post_id, all_data[i])
     """"""
     print(f"len(change_pins) = {len(change_pins)}")
     print(f"change_pins = {change_pins}")
@@ -293,8 +293,8 @@ def new_concert_add(new_but_old_pins, new_data, all_data):
     print(f"len(new_data_filtered) = {len(new_data_filtered)}")
     print('-------------------------------------------------------------------------------------------------------')
     """"""
-    for i in range(len(new_data_filtered)):
-        post_concert(new_data_filtered[i])
+    # for i in range(len(new_data_filtered)):
+    #     post_concert(new_data_filtered[i])
     """"""
     return new_data_filtered, all_data
 
@@ -319,18 +319,18 @@ def old_concert_delete(old_but_new_pins, old_data, all_data):
     print('-------------------------------------------------------------------------------------------------------')
     print(f"old_but_new_pins = {old_but_new_pins}")
     """"""
-    with open('concert_pin_postid.txt', 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-    new_lines = []
-    for line in lines:
-        line_pin = line.split('|||')[1].strip()  # 使用strip()來移除尾部的換行符和其他空白
-        post_id = line.split('|||')[0]
-        if line_pin not in old_but_new_pins:
-            new_lines.append(line)
-        else:
-            delete_article(post_id)  # 假設 delete_article 是一個正確定義且可用來刪除文章的函數
-    with open('concert_pin_postid.txt', 'w', encoding='utf-8') as f:
-        f.writelines(new_lines)
+    # with open('concert_pin_postid.txt', 'r', encoding='utf-8') as f:
+    #     lines = f.readlines()
+    # new_lines = []
+    # for line in lines:
+    #     line_pin = line.split('|||')[1].strip()  # 使用strip()來移除尾部的換行符和其他空白
+    #     post_id = line.split('|||')[0]
+    #     if line_pin not in old_but_new_pins:
+    #         new_lines.append(line)
+    #     else:
+    #         delete_article(post_id)  # 假設 delete_article 是一個正確定義且可用來刪除文章的函數
+    # with open('concert_pin_postid.txt', 'w', encoding='utf-8') as f:
+    #     f.writelines(new_lines)
     """"""
     return all_data
 
