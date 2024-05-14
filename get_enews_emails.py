20d3292613b286f6a760159040dd9bf200f06f5b
akuan1997 <akuan10cc@gmail.com>
2024-05-15 06:47:13 +0800

    emails okay

--- get_enews_emails.py
+++ get_enews_emails.py
@@ -1,34 +1,40 @@
 from playwright.sync_api import sync_playwright, Playwright
 from playwright.sync_api import expect, Page
 
+
 def get_enews_emails():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        # browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
-
+        wait_seconds = 3000
         page.goto('https://concertinfo.site/wp-admin')
 
         page.locator("#user_login").fill('user')
         page.locator("#user_pass").fill('fjKuan123!')
-        page.wait_for_timeout(1500)
+        page.wait_for_timeout(wait_seconds)
         page.keyboard.press('Enter')
-        page.wait_for_load_state('load')
-        page.wait_for_timeout(3000)
+        # page.wait_for_load_state('load')
+        page.wait_for_timeout(wait_seconds)
+        page.wait_for_selector(".wp-menu-name")
         menu_names = page.query_selector_all(".wp-menu-name")
         print(len(menu_names))
         for i in range(len(menu_names)):
             if 'CRM Entries' in menu_names[i].text_content():
                 break
         menu_names[i].click()
-        page.wait_for_load_state('load')
-        page.wait_for_timeout(1500)
+        # page.wait_for_load_state('load')
+        page.wait_for_timeout(wait_seconds)
+        page.wait_for_selector("#entries_form")
         page.locator("#entries_form").click()
-        page.wait_for_load_state('load')
-        page.wait_for_timeout(1500)
+        # page.wait_for_load_state('load')
+        page.wait_for_timeout(wait_seconds)
+        page.wait_for_selector("#entries_form")
         page.locator("#entries_form").select_option(value="wp_393")
-        page.wait_for_load_state('load')
-        page.wait_for_timeout(1500)
+        # page.wait_for_load_state('load')
+        page.wait_for_timeout(wait_seconds)
+        page.wait_for_selector("#vx_entries_table > tbody > tr")
         emails = page.query_selector_all("#vx_entries_table > tbody > tr")
         enews_emails = []
         for i in range(len(emails)):
@@ -37,6 +43,6 @@ def get_enews_emails():
             enews_emails.append(email)
             print(email)
         print(enews_emails)
-        return set(enews_emails)
+        return list(set(enews_emails))
 
 # get_enews_emails()
