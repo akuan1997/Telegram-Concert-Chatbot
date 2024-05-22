acb25818c890fdc049dcdf77b19f97a1df0cd02a
akuan1997 <akuan10cc@gmail.com>
2024-05-23 00:11:56 +0800

    240523

--- git_auto_push.bat
+++ git_auto_push.bat
@@ -1,6 +1,8 @@
 @echo off
 cd C:\Users\pfii1\akuan\git-repos\2024_Concert_Chatbot
-set currentDate=%date:~0,4%%date:~5,2%%date:~8,2%
+set "year=%date:~8,2%"
+set "month=%date:~0,2%"
+set "day=%date:~3,2%"
 git add .
-git commit -m "%currentDate%"
+git commit -m "%year%%month%%day%"
 git push
